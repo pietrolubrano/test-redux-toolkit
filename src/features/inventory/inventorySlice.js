c29c7c4a59807a0a039cb5ca35f75aed0ae425a6
Claudio <claudio@DESKTOP-HA26NJA.localdomain>
2021-07-09 09:28:45 +0200

    Cleaned duplicate of inventory importing it from database to inventoryslice and setting to initialState

--- src/features/inventory/inventorySlice.js
+++ src/features/inventory/inventorySlice.js
@@ -1,60 +1,24 @@
-import { createSlice } from '@reduxjs/toolkit';
-
+import { createSlice } from "@reduxjs/toolkit";
+import { Products } from "../../app/database";
 export const slice = createSlice({
-  name: 'inventory',
-  initialState: [
-    {
-        id: 0,
-        label: 'mouse',
-        price: 10,
-        stock: 15,
-    },
-    {
-        id: 1,
-        label: 'keyboard',
-        price: 15,
-        stock: 1,
-    },
-    {   
-        id: 2,
-        label: 'headphones',
-        price: 60,
-        stock: 0,
-    },
-    {
-        id: 3,
-        label: 'laptop',
-        price: 250,
-        stock: 5,
-    },
-    {
-        id: 4,
-        label: 'cuffie',
-        price: 200,
-        stock: 3,
-    },
-    {
-        id: 5,
-        label: 'sedia',
-        price: 40,
-        stock: 7,
-    },
-],
+  name: "inventory",
+  initialState: [...Products],
   reducers: {
     addProductToInventory: (state, action) => {
       const product = action.payload.product;
-      state.push(product)
-      console.log("state", state.cart)
-      console.log("action payload", action.payload.product)
+      state.push(product);
+      console.log("state", state.cart);
+      console.log("action payload", action.payload.product);
     },
     removeProductFromInventory: (state, action) => {
       const product = action.payload.product;
       state[product.id].stock--;
-    }
+    },
   },
 });
 
-export const { addProductToInventory, removeProductFromInventory } = slice.actions;
+export const { addProductToInventory, removeProductFromInventory } =
+  slice.actions;
 
 // The function below is called a thunk and allows us to perform async logic. It
 // can be dispatched like a regular action: `dispatch(incrementAsync(10))`. This
@@ -64,6 +28,6 @@ export const { addProductToInventory, removeProductFromInventory } = slice.actio
 // The function below is called a selector and allows us to select a value from
 // the state. Selectors can also be defined inline where they're used instead of
 // in the slice file. For example: `useSelector((state) => state.counter.value)`
-export const selectInventory = state => state.inventory;
+export const selectInventory = (state) => state.inventory;
 
 export default slice.reducer;
